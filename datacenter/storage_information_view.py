61873c7bd6a721deaa39b82f77f82e1c3884f392
Andrey9045 <andreidikun123@gmail.com>
2025-05-02 19:16:02 +0400

    Отформатировал код согласно рекомендациям Devman

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -13,9 +13,14 @@ def storage_information_view(request):
         {
             'who_entered': str(visit.passcard),
             'entered_at': visit.entered_at.strftime('%d %B %Y г. %H:%M'),
-            'duration': format_duration(get_duration(visit), seconds_in_minute=60, seconds_in_hour=3600),
+            'duration': format_duration(
+                get_duration(visit),
+                seconds_in_minute=60,
+                seconds_in_hour=3600,
+            ),
         }
-    for visit in visits]
+        for visit in visits
+    ]
     context = {
         'non_closed_visits': non_closed_visits,
     }
